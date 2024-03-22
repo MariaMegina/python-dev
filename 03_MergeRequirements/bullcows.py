2e1f17c42f31c12268c0b375fd20fc16bf7c3814
Maria <meginam16@gmail.com>
2024-03-22 19:20:07 +0300

    Added bullcows

---
+++ 03_MergeRequirements/bullcows.py
@@ -0,0 +1,60 @@
+import sys
+import random
+import urllib.request
+
+def bullscows(guess, secret):
+    bulls = 0
+    cows = 0
+    for i in range(len(guess)):
+        if guess[i] == secret[i]:
+            bulls += 1
+        elif guess[i] in secret:
+            cows += 1
+    return (bulls, cows)
+
+def gameplay(ask, inform, words):
+    attempts = 0
+    secret = random.choice(words)
+    while True:
+        guess = ask("Введите слово: ", words)
+        bulls, cows = bullscows(guess, secret)
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+        attempts += 1
+        if guess == secret:
+            return attempts
+
+
+def inform(format_string, bulls, cows):
+    print(format_string.format(bulls, cows))
+
+
+def ask(prompt, valid = None):
+    guess = input(prompt)
+    if valid != None:
+        while guess not in valid:
+            guess = input(prompt)
+    return guess
+
+if __name__ == "__main__":
+
+    if sys.argv[1].startswith("https://"):
+        dictionary = urllib.request.urlopen(sys.argv[1]).read().decode("utf-8").splitlines()
+
+    else:
+        try:
+            with open(sys.argv[1], "r") as file:
+                dictionary = [word.strip() for word in file]
+        except Exception:
+            print("dictionary is not corret")
+            sys.exit(1)
+    if len(sys.argv) == 2:
+        length = 5
+    elif len(sys.argv) == 3:
+        length = int(sys.argv[2])
+    else:
+        print("arguments is not correct")
+        sys.exit(1)
+
+    dictionary = [word for word in dictionary if len(word) == length]
+    print(dictionary)
+    print("Попыток: ",gameplay(ask, inform, dictionary))
