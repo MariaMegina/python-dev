0222384ea79aaa5cc2963c5c1cf16844ce5f8c78
Maria <meginam16@gmail.com>
2024-03-22 13:50:15 +0300

    Added cowfile

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -5,11 +5,12 @@ import argparse
 cowparser = argparse.ArgumentParser(prog="cow_say")
 cowparser.add_argument("message", nargs="?")
 cowparser.add_argument("-e", "--eye_string", dest="eyes", default=cowsay.Option.eyes)
+cowparser.add_argument("-f", "--cowfile")
 cowparser.add_argument("-l", action="store_true")
 cowargs = cowparser.parse_args()
 
 if cowargs.l:
     print(cowsay.list_cows())
 else:
-    print(cowsay.cowsay(cowargs.message, eyes=cowargs.eyes))
+    print(cowsay.cowsay(cowargs.message, eyes=cowargs.eyes, cowfile=cowargs.cowfile))
 
